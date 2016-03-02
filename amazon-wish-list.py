4714806530854c4a5b403bd449ff22cbfc95aa1e
Kevin Chou <KChou@precima.com>
2016-03-02 13:20:06 -0500

    created function to get product ASIN from wishlist

--- amazon-wish-list.py
+++ amazon-wish-list.py
@@ -2,38 +2,42 @@ import re
 import urllib.request
 from bs4 import BeautifulSoup
 
-wishlist = '108CUW1LYHCYJ'
-amazon_url = 'http://www.amazon.ca/registry/wishlist/'
 
-url = amazon_url + wishlist
+WISHLIST_URL = 'http://www.amazon.ca/registry/wishlist/'
 
-request = urllib.request.urlopen(url)
-
-# if request.code == 200:
-raw_html = request.read()
-
-soup = BeautifulSoup(raw_html, 'lxml')
-
-items = soup.find_all('div', {'id':re.compile("item_\S+")})
-
-for item in items:
-
-    id_string = item.attrs['id']
-
-    id = id_string[id_string.find('_')+1:]
-    title = item.find('a', {'id':'itemName_{id}'.format(id=id)}).attrs['title']
-
-    item.find('span', {'id':'itemPrice_{id}'.format(id=id)}).contents
-    
-    print(id, title)
-
-    
+wishlist_id = '108CUW1LYHCYJ'
 
 
-id_start = item_id.index('_') + 1
+def get_wishlist_items(wishlist_id):
+    ''' Gets the ASIN item identifier of all items in wishlist '''
 
-item_id[id_start:]
+    wishlist_items = []
 
+    url = WISHLIST_URL + wishlist_id
+    request = urllib.request.urlopen(url)
 
+    if request.code == 200:
+        raw_html = request.read()
 
+        soup = BeautifulSoup(raw_html, 'lxml')
 
+        items = soup.find_all('div', {'id': re.compile("item_\S+")})
+
+        for item in items:
+
+            id_string = item.attrs['id']
+            asin = id_string[id_string.find('_')+1:]
+
+            wishlist_items.append(asin)
+
+    return wishlist_items
+
+get_wishlist_items(wishlist_id)
+
+'''
+title = item.find('a', {'id': 'itemName_'}).attrs['title']
+
+p = item.find('span', {'id': 'itemPrice_'})
+
+print(id, title, price_string)
+'''
